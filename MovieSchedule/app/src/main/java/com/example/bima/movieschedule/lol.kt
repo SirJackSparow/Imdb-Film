cdc12523ee5e08328b5c0d290ebb2fbfbe4890cd
Bima <bima@mymindstores.com>
2019-07-06 12:16:55 +0700

    menambah lol

---
+++ MovieSchedule/app/src/main/java/com/example/bima/movieschedule/lol.kt
@@ -0,0 +1,12 @@
+package com.example.bima.movieschedule
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class lol : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
